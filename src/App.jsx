189828b9b3aff27eacde2d9766456c08491d7fc0
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-03-31 09:51:12 +0600

    Home and sidebar layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="main grid grid-cols-1 md:grid-cols-4 gap-10 md:mx-64 text-center">
+        <div className="home-container md:col-span-3 bg-blue-500">
+          <Home />
+        </div>
+        <div className="sidebar-container md:col-span-1 bg-red-500">
+          <Sidebar />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/SingleCard/SingleCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SingleCard = () => {
+  return (
+    <div>
+      <h1>single card</h1>
+    </div>
+  );
+};
+
+export default SingleCard;
